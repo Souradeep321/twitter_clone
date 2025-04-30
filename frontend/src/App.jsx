89863b22bc3dd99d8af16ebc314d5a90500f35e1
Souradeep321 <souradeephazra93@gmail.com>
2025-04-30 16:02:28 +0530

    Ui design completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+
+function App() {
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			<Sidebar /> {/* Common Components */}
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+			<RightPanel /> {/* Common Components */}
+		</div>
+	);
+}
+
+export default App;
